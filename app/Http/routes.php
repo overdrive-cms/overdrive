9a0294bbf48f12118084094581ab1683bc518fa0
Adrian Goldner <adrian.goldner@gmail.com>
2015-01-29 23:19:09 +0100

    Added Modules Folder

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('test', function() {
+	return 'Moin moin!';
+});
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
