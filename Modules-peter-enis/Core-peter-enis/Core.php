7a8e91fbd9cf825cabd50840c7b3440ce2a65e50
Mr. T <halbi93@gmx.de>
2015-02-05 22:02:23 +0100

    Made backup of Modules (the old one) ...

---
+++ Modules-peter-enis/Core-peter-enis/Core.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: adrian
+ * Date: 05.02.15
+ * Time: 02:55
+ */
+
+namespace Modules\Core;
+
+
+class Core {
+	public function start()
+	{
+		//
+	}
+}
